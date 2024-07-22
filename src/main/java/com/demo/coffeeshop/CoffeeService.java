b5e8b8bf208ea44939e53a450e8fa1d820958faf
anna <anna.hrunova93@gmail.com>
2024-07-22 17:56:44 +0300

    new code

---
+++ src/main/java/com/demo/coffeeshop/CoffeeService.java
@@ -0,0 +1,29 @@
+package com.demo.coffeeshop;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CoffeeService {
+    @Autowired
+    private CoffeeRepository coffeeRepository;
+
+    public List<CoffeeEntity> getAllCoffees() {
+        return coffeeRepository.findAll();
+    }
+
+    public CoffeeEntity getCoffeeById(Long id) {
+        return coffeeRepository.findById(id).orElse(null);
+    }
+
+    public CoffeeEntity saveCoffee(CoffeeEntity coffee) {
+        return coffeeRepository.save(coffee);
+    }
+
+    public void deleteCoffee(Long id) {
+        coffeeRepository.deleteById(id);
+    }
+}
+
