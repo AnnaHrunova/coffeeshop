d898852ee7609b73ee4bede2e5a1e0dedfdc4c5e
anna <anna.hrunova93@gmail.com>
2024-07-23 16:21:45 +0300

    controller, valid

--- src/main/java/com/demo/coffeeshop/Controller.java
+++ src/main/java/com/demo/coffeeshop/Controller.java
@@ -2,6 +2,7 @@ package com.demo.coffeeshop;
 
 import com.demo.coffeeshop.domain.CoffeeEntity;
 import com.demo.coffeeshop.domain.CoffeeRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
